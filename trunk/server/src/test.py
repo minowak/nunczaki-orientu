4b0ef1c5277e9964bd6e3730fceef0d35b810def
alejajestemmondry@gmail.com <alejajestemmondry@gmail.com@4aaf7020-baa3-cf0f-5c26-0009847aa3da>
2011-10-17 19:37:45 +0000

    Added simple bluetooth support (working).

--- trunk/server/src/test.py
+++ trunk/server/src/test.py
@@ -1,4 +1,18 @@
 import cv
+import bluetooth
+
+# discovering available devices (this may take a sec)
+devices = bluetooth.discover_devices()
+
+# print available devices
+for dev in devices:
+    print '%s: %s' % (dev, bluetooth.lookup_name(dev))
+    services = bluetooth.find_service(address=dev)
+    
+    for i in services:
+        print i
+        
+    print
 
 capture = cv.CaptureFromCAM(0)
 
@@ -8,6 +22,6 @@ while count < 25:
     image = cv.QueryFrame(capture)
     
     cv.ShowImage('Test WebCam in Python', image)
-
+    cv.SaveImage('test_image.jpg', image)
     cv.WaitKey(2)
     count += 1
