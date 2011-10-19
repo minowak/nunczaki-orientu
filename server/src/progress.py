c47c0b1ea191b0d8b35e29f502b1b40dfd22b4e2
alejajestemmondry@gmail.com <alejajestemmondry@gmail.com@4aaf7020-baa3-cf0f-5c26-0009847aa3da>
2011-10-19 20:15:39 +0000

    Searching in another thread. Added status bar.

---
+++ server/src/progress.py
@@ -0,0 +1,30 @@
+import bluetooth
+import gtk
+import threading
+
+class ProgressThread(threading.Thread):
+    def __init__(self, devs):
+        threading.Thread.__init__(self)
+        gtk.threads_init()
+        self.base = devs
+        
+    def run(self):
+        print 'starting searching thread'
+        self.base.statusbar.push(0, 'Searching ...')
+        devices = bluetooth.discover_devices()
+        print 'end searching'
+        
+        found = 0
+        
+        for dev in devices:
+            found += 1
+            self.base.liststore.append([str(bluetooth.lookup_name(dev)), str(dev)])
+            
+        print 'showing devices'
+
+        self.base.button1.set_sensitive(True)
+        
+        if found > 0:
+            self.base.statusbar.push(0, 'Found ' + str(found) + ' devices')
+        else:
+            self.base.statusbar.push(0, 'No devices found')
