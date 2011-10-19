4c030afb43d0f81d01acc3b3d81ea595b141cb60
alejajestemmondry@gmail.com <alejajestemmondry@gmail.com@4aaf7020-baa3-cf0f-5c26-0009847aa3da>
2011-10-19 20:15:39 +0000

    Searching in another thread. Added status bar.

--- trunk/server/src/test.py
+++ trunk/server/src/test.py
@@ -1,16 +1,16 @@
-import cv
-import bluetooth
-
 import pygtk
 pygtk.require('2.0')
 import gtk
 
-class Base:
-    def search_devices(self, widget, data=None):
-        devices = bluetooth.discover_devices()
+from progress import ProgressThread
+
+# CLASSES
         
-        for dev in devices:
-            self.liststore.append([str(bluetooth.lookup_name(dev)), str(dev)])
+class Base:    
+    def search_devices(self, widget, data=None):
+        self.button1.set_sensitive(False)        
+        progress_thread = ProgressThread(self)
+        progress_thread.start()
     
     def __init__(self):
         # VARIABLE INITS
@@ -20,32 +20,39 @@ class Base:
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title("Nunczaki Orientu")
         self.window.set_border_width(10)
-        self.window.set_size_request(300, 400)
+        self.window.set_size_request(450, 400)
         
         self.window.connect("delete_event", self.delete_event)
         self.window.connect("destroy", self.destroy)
     
-        # BUTTONS etc
+        # BUTTONS
         
         self.button1 = gtk.Button("Search Devices")
         self.button1.connect("clicked", self.search_devices)
         
+        # LISTVIEW
+        
         self.liststore = gtk.ListStore( str, str )
         self.treeview = gtk.TreeView(self.liststore)
         
-        self.tvcolumn = gtk.TreeViewColumn('Device name')
+        self.tvcolumn = gtk.TreeViewColumn('Device name                   ') # lol i don't wanna search for another solution :P
         self.tvcolumn2 = gtk.TreeViewColumn('Address')
         
         self.treeview.append_column(self.tvcolumn)
         self.treeview.append_column(self.tvcolumn2)
         
-        self.cell = gtk.CellRendererText()
+        cell = gtk.CellRendererText()
     
-        self.tvcolumn.pack_start(self.cell, True)
-        self.tvcolumn2.pack_start(self.cell, True)
+        self.tvcolumn.pack_start(cell, True)
+        self.tvcolumn2.pack_start(cell, True)
         
-        self.tvcolumn.set_attributes(self.cell, text=0)
-        self.tvcolumn2.set_attributes(self.cell, text=1)
+        self.tvcolumn.set_attributes(cell, text=0)
+        self.tvcolumn2.set_attributes(cell, text=1)
+        
+        # STATUSBAR
+        
+        self.statusbar = gtk.Statusbar()
+        self.statusbar.push(0, 'Welcome')
     
         # LAYOUTS
         
@@ -57,11 +64,8 @@ class Base:
         self.vscrollbar.add(self.treeview)
 
         self.box1.pack_start(self.vscrollbar, True, True, 0)
-        self.box1.pack_start(self.button1, True, True, 0)
-        
-#        self.button1.show()
-#        self.box1.show()
-#        self.devices_list.show()
+        self.box1.pack_start(self.button1, False, True, 0)
+        self.box1.pack_start(self.statusbar, False, True, 0)
         
         self.window.show_all()
         print 'main window started'
