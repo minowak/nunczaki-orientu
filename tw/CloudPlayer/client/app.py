998d62d3f3c94d0e9622e3ed4080b12031738e4e
alejajestemmondry@gmail.com <alejajestemmondry@gmail.com@4aaf7020-baa3-cf0f-5c26-0009847aa3da>
2011-12-02 18:14:39 +0000

    Layouty ułożone + obrazki na przyciski

--- tw/CloudPlayer/client/app.py
+++ tw/CloudPlayer/client/app.py
@@ -20,27 +20,57 @@ class AppBase:
         print "Stop"
     
     def initGUI(self):
-        self.HBox = gtk.HBox(False, 0)
+        # Boxes
+        self.HBox = gtk.HBox(False, 10)
+        self.VBox = gtk.VBox(False, 5)
         
-        self.playBtn = gtk.Button("Play")
-        self.stopBtn = gtk.Button("Stop")
+        # Images
+        playImg = gtk.Image()
+        playImg.set_from_file("play.png")
+        playImg.show()
+        
+        stopImg = gtk.Image()
+        stopImg.set_from_file("stop.png")
+        stopImg.show()
+        
+        # Buttons
+        self.playBtn = gtk.Button()
+        self.playBtn.add(playImg)
+        self.playBtn.set_size_request(40, 40)
+        
+        self.stopBtn = gtk.Button()
+        self.stopBtn.add(stopImg)
+        self.stopBtn.set_size_request(40, 40)
+        
+        # Scale
+        self.scale = gtk.HScale()
+        self.scale.set_digits(2)
+        self.scale.set_draw_value(0)
+        self.scale.set_size_request(250, 10)
+        self.scale.show()
         
         # Connects
         self.playBtn.connect_object("clicked", self.play_callback, None)
         self.stopBtn.connect_object("clicked", self.stop_callback, None)
         
-        self.HBox.add(self.playBtn)
-        self.HBox.add(self.stopBtn)
+        # Packing
+        self.HBox.pack_start(self.playBtn, False, False, 0)
+        self.HBox.pack_start(self.stopBtn, False, False, 0)
+        self.HBox.add(self.scale)
         self.HBox.show_all()
+        
+        self.VBox.pack_start(self.HBox, False, False, 5)
+        self.VBox.show();
         
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_default_size(20,20)
         self.window.set_title("Cloud PLayer")
         self.window.show()
         
         self.initGUI()
         
-        self.window.add(self.HBox)
+        self.window.add(self.VBox)
         self.window.set_border_width(5)
         self.window.connect("destroy", self.destroy)
         
